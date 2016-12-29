cf69ebd80363203a69bf3b338fb2cadd840f230d
Adam Farden <adam@farden.cz>
2016-12-29 11:17:51 +0100

    Use CM's adreno blobs

--- qcom/prebuilt/qcom-vendor.mk
+++ qcom/prebuilt/qcom-vendor.mk
@@ -1,4 +1,4 @@
 $(call inherit-product, vendor/qcom/prebuilt/qcom-partial-msm8226.mk)
-$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-adreno-a3xx.mk)
+$(call inherit-product, vendor/qcom/binaries/msm8974/graphics/graphics-vendor.mk)
 $(call inherit-product, vendor/qcom/proprietary/yukon-extras.mk)
 $(call inherit-product, vendor/qcom/firmware/qcom-modem-firmware.mk)
