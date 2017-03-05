4062202588ea5179e7add5592a619639437a656c
kpliakas <kjpliakas@gmail.com>
2017-03-05 14:50:03 +0200

    Eagle extra blobs

--- eagle/eagle-vendor.mk
+++ eagle/eagle-vendor.mk
@@ -1,3 +1,4 @@
 PRODUCT_PLATFORM := yukon
 $(call inherit-product, vendor/sony/yukon/yukon-vendor.mk)
 $(call inherit-product, vendor/sony/eagle/eagle-partial.mk)
+$(call inherit-product, vendor/sony/yukon-eagle/eagle-stock.mk)
