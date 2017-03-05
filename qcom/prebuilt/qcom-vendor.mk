4f4df4dba985255ccda46c7a294b78fa86735b7d
kpliakas <kjpliakas@gmail.com>
2017-03-05 14:30:09 +0200

    Revert "Use CM's adreno blobs"
    This reverts commit cf69ebd80363203a69bf3b338fb2cadd840f230d.

---
+++ qcom/prebuilt/qcom-vendor.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-msm8226.mk)
+$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-adreno-a3xx.mk)
+$(call inherit-product, vendor/qcom/proprietary/yukon-extras.mk)
+$(call inherit-product, vendor/qcom/firmware/qcom-modem-firmware.mk)
