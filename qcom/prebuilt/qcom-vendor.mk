354a42ab8fbf8fca616c4899225be68eb24995ba
kpliakas <kjpliakas@gmail.com>
2017-03-05 14:32:36 +0200

    Clean-up Adreno blobs

--- qcom/prebuilt/qcom-vendor.mk
+++ qcom/prebuilt/qcom-vendor.mk
@@ -1,4 +1,1 @@
-$(call inherit-product, vendor/qcom/prebuilt/qcom-partial-msm8226.mk)
 $(call inherit-product, vendor/qcom/prebuilt/qcom-partial-adreno-a3xx.mk)
-$(call inherit-product, vendor/qcom/proprietary/yukon-extras.mk)
-$(call inherit-product, vendor/qcom/firmware/qcom-modem-firmware.mk)
