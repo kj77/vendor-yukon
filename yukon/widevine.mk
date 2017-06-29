36cc2ff2aa0963558b23d050dfbeaa753965208b
kpliakas <kjpliakas@gmail.com>
2017-06-29 15:13:47 +0300

    Add widevine DRM Files Taken from https://github.com/TheMuppets/proprietary_vendor_sony/commit/9fa4b5015669928d284d5ee7b15610e5a37389af#diff-96d00f7bda78012cc8bffc3aeebe8bc8L132

---
+++ yukon/widevine.mk
@@ -0,0 +1,8 @@
+# Widevine DRM
+PRODUCT_COPY_FILES += \
+    vendor/sony/yukon/proprietary/lib/drm/libdrmwvmplugin.so:system/lib/drm/libdrmwvmplugin.so \
+    vendor/sony/yukon/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/sony/yukon/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/sony/yukon/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/sony/yukon/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+
--- yukon/yukon-vendor.mk
+++ yukon/yukon-vendor.mk
@@ -1,3 +1,5 @@
 $(call inherit-product, vendor/sony/yukon/yukon-partial.mk)
 $(call inherit-product, vendor/sony/yukon/yukon-extras.mk)
+$(call inherit-product, vendor/sony/yukon/widevine.mk)
 $(call inherit-product, vendor/sony/qcom/prebuilt/qcom-vendor.mk)
+
