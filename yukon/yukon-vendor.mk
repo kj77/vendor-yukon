b654ef44ca4c159e1c9255e169e8c22a70490574
galaxyfreak <marinspajic17@hotmail.com>
2017-09-19 18:33:10 +0200

    Fix Widewine DRM
    Take blob from angler

--- yukon/yukon-vendor.mk
+++ yukon/yukon-vendor.mk
@@ -1,5 +1,4 @@
 $(call inherit-product, vendor/sony/yukon/yukon-partial.mk)
 $(call inherit-product, vendor/sony/yukon/yukon-extras.mk)
-$(call inherit-product, vendor/sony/yukon/widevine.mk)
 $(call inherit-product, vendor/sony/qcom/prebuilt/qcom-vendor.mk)
 
